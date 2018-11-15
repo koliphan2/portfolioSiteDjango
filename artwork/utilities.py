38abfe30cd4ad21d24415df24df589431c9b0265
Katie <katherine.oliphant@quansight.com>
2018-11-15 12:15:16 -0700

    Added thumbnail utility

--- artwork/utilities.py
+++ artwork/utilities.py
@@ -1,12 +1,23 @@
 from .models import webContent
+from PIL import image
+import os
 
 
 # Model Functions
-def getDictionary(propertyName=None):
-        # deal with property name if added
-        allObjects = webContent()
-        dictionary = {}
-        for obj in allObjects.objects.filter():
-            dictionary[obj.propertyName] = obj.proertyValue
-        return dictionary
+def get_dictionary(propertyName=None):
+    # deal with property name if added
+    allObjects = webContent()
+    dictionary = {}
+    for obj in allObjects.objects.filter():
+        dictionary[obj.propertyName] = obj.proertyValue
+    return dictionary
 
+
+def make_thumbnail(file_name):
+    size_200 = (200,200)
+
+    i = image.open(file_name)
+    fn, fext = os.path.splitext(file_name)
+
+    i.thumbnail(size_200)
+    i.save('./media/thumbnails/{}_thumbnail.{}'.format(fn, fext))
